6d3724530272afb9bd5771912ad9ad3349f47a6b
thelightway <thelightway@daum.net>
2025-04-07 10:57:49 +0900

    2025-04-07 수업

--- src/main/java/com/inspire12/likelionbackend/module/order/infrastructure/adpter/OrderRepositoryAdapter.java
+++ src/main/java/com/inspire12/likelionbackend/module/order/infrastructure/adpter/OrderRepositoryAdapter.java
@@ -9,6 +9,8 @@ import com.inspire12.likelionbackend.module.order.infrastructure.repository.enti
 import com.inspire12.likelionbackend.module.order.support.mapper.OrderMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public class OrderRepositoryAdapter implements OrderRepository {
 
@@ -22,7 +24,13 @@ public class OrderRepositoryAdapter implements OrderRepository {
 
     public Order getOrderByOrderId(Long orderId) {
         // TODO:1 가계 도메인한테(책임을 위임해서) 가계가 열렸는지 확인한다
+        OrderEntity order = orderJpaRepository.findById(orderId)
+                            .orElseThrow(OrderNotExistException::new);
+        Boolean isOpenStore = statusPort.getStoreOpenStatus(order.getStoreId());
         // TODO:2 가계가 열렸으면 orderId로 주문을 가져와 도메인 객체로 변환해 반환한다
+        if(isOpenStore){
+            return OrderMapper.fromEntity(order);
+        }
 
         throw new OrderNotExistException();
     }
